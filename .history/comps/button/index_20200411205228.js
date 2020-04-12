b2c3f15b89909b93c834da81266759e162898fb9
joshrenema <josh.renema25@gmail.com>
2020-04-11 21:53:33 -0700

    updated button

---
+++ .history/comps/button/index_20200411205228.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./button.css";
+
+const Button = ({text, color, onClick}) => <div 
+style={{backgroundColor:color }}
+className="button_box"
+onClick={onClick}
+>
+    <div className="button_box_inner">
+        {text}
+    </div>
+</div>
+
+Button.defaultProps = {
+    text:"Default Button",
+    color:"#f2f2f2",
+    :hover{
+        color:"#28524b"
+    }
+}
+
+export default Button;
